2ceaeeaf7ca692412e81c8d5d5185d0d2018b140
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-04-24 10:36:29 +0200

    clissic

--- src/main/java/fr/perso/mvc/classic/Bakery.java
+++ src/main/java/fr/perso/mvc/classic/Bakery.java
@@ -1,4 +1,34 @@
 package fr.perso.mvc.classic;
 
+import java.util.List;
+
 public class Bakery {
+    private String name;
+    private List<String> products;
+    private final Address address;
+
+    public Bakery(Address address) {
+        this.address = address;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<String> products) {
+        this.products = products;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
 }
--- src/main/java/fr/perso/mvc/classic/MainClassic.java
+++ src/main/java/fr/perso/mvc/classic/MainClassic.java
@@ -3,6 +3,7 @@ package fr.perso.mvc.classic;
 public class MainClassic {
 
     public static void main(String[] args) {
-
+        Address address=new Address("Monopoly", "Paris", 95000);
+        Bakery bakery=new Bakery(address);
     }
 }
